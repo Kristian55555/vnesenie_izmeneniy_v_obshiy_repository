03713a8ccd2d7b39d94f476e8a4859c6e3e2a926
Kristian55555 <94179119+Kristian55555@users.noreply.github.com>
2021-12-20 09:55:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,21 @@ print('A * x^2 + B * x + C = 0\nВведите коэффициенты квад
 a = float(input('A = '))
 b = float(input('B = '))
 c = float(input('C = '))
-
-#Вставить свой код здесь
+import math
+print('A * x^2 + B * x + C = 0\nВведите коэффициенты квадратного уравнения:')
+A = float(input('A = '))
+B = float(input('B = '))
+C = float(input('C = '))
+ 
+D = B * B - 4 * A * C
+print("Дискриминант D = " , D)
+if D >0:
+    X1 = (- B + math.sqrt(D))/2 * A
+    X2 = (- B - math.sqrt(D))/2 * A
+    print("X1 = " , X1)
+    print("X2 = " , X2)
+elif D == 0:      
+    X = - B / 2 * A
+    print("X = " , X)
+else:
+    print("Вещественных корней нет")
